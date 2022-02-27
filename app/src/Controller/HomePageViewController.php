00d042d3bb8db608bc65b37f5ce0ef40277087d8
ejjoj <szymaskibartosz@gmail.com>
2022-02-27 21:10:10 +0100

    dashboard - refactoring

--- app/src/Controller/HomePageViewController.php
+++ app/src/Controller/HomePageViewController.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomePageController extends AbstractController
+class HomePageViewController extends AbstractController
 {
     /**
      * @Route("/", name="front.home_page")
--- app/src/Controller/Patient/Register/PatientRegisterViewController.php
+++ app/src/Controller/Patient/Register/PatientRegisterViewController.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class RegisterPatientViewController extends AbstractController
+class PatientRegisterViewController extends AbstractController
 {
     /**
      * @Route("/patient/register", name="front.patient.register")
