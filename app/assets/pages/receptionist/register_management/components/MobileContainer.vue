99dbe2272a6e87c4ac9a97dcb196bd9b7bdb5aec
ejjoj <szymaskibartosz@gmail.com>
2022-03-06 13:30:04 +0100

    receptionist - register management - mobile preparation

---
+++ app/assets/pages/receptionist/register_management/components/MobileContainer.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-container
+    :style="{
+      paddingTop: `${navbarHeight}px`,
+      minHeight: `calc(100vh - ${footerHeight}px)`,
+    }"
+  >
+    <app-background color-schema="receptionist" />
+
+    <route-screen
+      header="Zarządzanie rejestracją użytkowników"
+      text="Na poniższej liście znajdują się oczekujące prośby akceptacji o rejestrację. Zweryfikuj dane i zaakceptuj lub odrzuć dane podanie."
+    />
+  </v-container>
+</template>
+
+<script>
+import AppBackground from '../../../../shared/components/AppBackground';
+import RouteScreen from '../../../../shared/components/RouteScreen';
+
+export default {
+  name: 'MobileContainer',
+  components: { AppBackground, RouteScreen },
+  props: {
+    navbarHeight: {
+      type: Number,
+      required: true,
+    },
+    footerHeight: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  position: relative;
+  z-index: 2;
+  overflow-x: hidden;
+}
+</style>
--- app/assets/pages/receptionist/register_management/components/ReceptionistRegisterManagement.vue
+++ app/assets/pages/receptionist/register_management/components/ReceptionistRegisterManagement.vue
@@ -1,12 +1,34 @@
 <template>
   <v-app>
-    hello
+    <app-navbar
+      ref="navbar"
+      color-schema="receptionist"
+    />
+
+    <mobile-container
+      v-if="breakpoint.mdAndDown"
+      :navbar-height="navbarHeight"
+      :footer-height="footerHeight"
+    />
+
+    <app-footer
+      ref="footer"
+      color-schema="receptionist"
+    />
   </v-app>
 </template>
 
 <script>
+import AppNavbar from '../../../../shared/components/AppNavbar';
+import AppFooter from '../../../../shared/components/AppFooter';
+import breakpoint from '../../../../mixins/breakpoint';
+import mainComponentHelper from '../../../../mixins/mainComponentHelper';
+import MobileContainer from './MobileContainer';
+
 export default {
   name: 'ReceptionistRegisterManagement',
+  components: { MobileContainer, AppFooter, AppNavbar },
+  mixins: [breakpoint, mainComponentHelper],
 };
 </script>
 
