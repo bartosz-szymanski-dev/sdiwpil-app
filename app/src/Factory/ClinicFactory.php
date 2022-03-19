461fe6ee4863aa38fe43f688ddbbf88fb7c4f293
ejjoj <szymaskibartosz@gmail.com>
2022-03-19 16:29:17 +0100

    Clinic - fixtures

---
+++ app/src/Factory/ClinicFactory.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Clinic;
+use App\Repository\ClinicRepository;
+use Zenstruck\Foundry\RepositoryProxy;
+use Zenstruck\Foundry\ModelFactory;
+use Zenstruck\Foundry\Proxy;
+
+/**
+ * @extends ModelFactory<Clinic>
+ *
+ * @method static Clinic|Proxy createOne(array $attributes = [])
+ * @method static Clinic[]|Proxy[] createMany(int $number, array|callable $attributes = [])
+ * @method static Clinic|Proxy find(object|array|mixed $criteria)
+ * @method static Clinic|Proxy findOrCreate(array $attributes)
+ * @method static Clinic|Proxy first(string $sortedField = 'id')
+ * @method static Clinic|Proxy last(string $sortedField = 'id')
+ * @method static Clinic|Proxy random(array $attributes = [])
+ * @method static Clinic|Proxy randomOrCreate(array $attributes = [])
+ * @method static Clinic[]|Proxy[] all()
+ * @method static Clinic[]|Proxy[] findBy(array $attributes)
+ * @method static Clinic[]|Proxy[] randomSet(int $number, array $attributes = [])
+ * @method static Clinic[]|Proxy[] randomRange(int $min, int $max, array $attributes = [])
+ * @method static ClinicRepository|RepositoryProxy repository()
+ * @method Clinic|Proxy create(array|callable $attributes = [])
+ */
+final class ClinicFactory extends ModelFactory
+{
+    protected function getDefaults(): array
+    {
+        return [
+            'name' => self::faker()->company(),
+            'country' => 'Polska',
+            'city' => 'Kraków',
+            'email' => self::faker()->email(),
+            'zipCode' => $this->getRandomCracowPostCode(),
+            'streetAddress' => self::faker()->streetAddress(),
+        ];
+    }
+
+    protected function initialize(): self
+    {
+        // see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+        return $this
+            // ->afterInstantiate(function(Clinic $clinic): void {})
+        ;
+    }
+
+    protected static function getClass(): string
+    {
+        return Clinic::class;
+    }
+
+    private function getRandomCracowPostCode(): string
+    {
+        $first = '3';
+        $secondArray = ['0', '1'];
+        $second = $secondArray[array_rand($secondArray)];
+        $result = sprintf('%s%s-', $first, $second);
+        for ($i = 0; $i < 3; $i++) {
+            $result .= random_int(0, 9);
+        }
+
+        return $result;
+    }
+}
