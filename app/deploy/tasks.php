1069cb30a0f3d5c4649f7b9ca0235375ab6f2e77
ejjoj <szymaskibartosz@gmail.com>
2022-03-27 19:44:36 +0200

    deployer

--- app/deploy/tasks.php
+++ app/deploy/tasks.php
@@ -7,6 +7,8 @@ set('console_options', function () {
     return '--no-interaction --env={{symfony_env}}';
 });
 
+task('deploy', ['install', 'configure', 'link']);
+
 task('deploy:vendors', function () {
     if (!commandExist('unzip')) {
         writeln('<comment>To speed up composer installation setup "unzip" command with PHP zip extension https://goo.gl/sxzFcD</comment>');
@@ -26,7 +28,7 @@ task('prepare', function () {
     set('branch', runLocally('git rev-parse --abbrev-ref HEAD'));
 
     $defaults = [
-        'shared_files' => ['.env'],
+        'shared_files' => ['app/.env'],
         'shared_dirs' => [
             'var/logs',
             'var/sessions',
@@ -36,11 +38,12 @@ task('prepare', function () {
         'bin/node' => 'node',
         'bin/npm' => 'npm',
         'bin/yarn' => 'yarn',
+        'symfony_cli' => 'symfony',
         'fastcgi_pass' => 'unix:/run/php/php7.4-fpm.sock',
         'nginx_config_dst_filename' => '', // TODO
         'bin_dir' => 'bin',
         'var_dir' => 'var',
-        'bin/console' => '{{release_path}}app/bin/console',
+        'bin/console' => '{{release_path}}/app/bin/console',
         'server_name' => '192.168.0.66',
         'parameters' => [
             'branch' => '{{branch}}',
@@ -58,40 +61,92 @@ task('prepare', function () {
     ->desc('Update configuration')
     ->once();
 
-task('configure:database:migration', function () {
-    run('{{bin/console}} doctrine:migrations:migrate --allow-no-migration');
+task('configure:database:migrate', function () {
+    run('{{symfony_cli}} doctrine:migrations:migrate --allow-no-migration');
 })
     ->desc('Database migrations');
 
 task('deploy:assets:install', function () {
-    run('{{bin/php}} {{bin/console}} assets:install {{console_options}} {{release_path}}/public');
+    run('{{symfony_cli}} assets:install {{console_options}} /public');
 })
     ->desc('Install bundle assets');
 
 task('configure:database:cache_clear', function () {
-    run('{{bin/php}} {{bin/console}} doctrine:cache:clear-metadata {{console_options}}');
-    run('{{bin/php}} {{bin/console}} doctrine:cache:clear-query {{console_options}}');
-    run('{{bin/php}} {{bin/console}} doctrine:cache:clear-result {{console_options}}');
+    run('{{symfony_cli}} doctrine:cache:clear-metadata {{console_options}}');
+    run('{{symfony_cli}} doctrine:cache:clear-query {{console_options}}');
+    run('{{symfony_cli}} doctrine:cache:clear-result {{console_options}}');
 })
     ->desc('Database cache clear');
 
 task('configure:webpack', function () {
     invoke('prepare');
 
-    run('{{bin/console}} fos:js-routing:dump --format=json --target={{release_path}}/app/public/js/fos_js_routes.json');
+    run('{{symofny_cli}} fos:js-routing:dump --format=json --target={{release_path}}/app/public/js/fos_js_routes.json');
     run('cd {{release_path}} && {{bin/yarn}} install --production');
     run('cd {{release_path}} && {{bin/npm}} run-script build');
 })
     ->desc('Configure webpack');
 
 task('configure:cache:clear', function () {
-    run('{{bin/php}} {{bin/console}} cache:clear {{console_options}} --no-warmup');
+    run('{{bin/php}} {{symfony_cli}} cache:clear {{console_options}} --no-warmup');
 })
     ->desc('Clear cache');
 
 task('configure:cache:warmup', function () {
-    run('{{bin/php}} {{bin/console}} cache:warmup {{console_options}}');
+    run('{{bin/php}} {{symfony_cli}} cache:warmup {{console_options}}');
 })
     ->desc('Warm up cache');
+
+task('install', function () {
+   invoke('prepare');
+
+   $tasks = [
+       'deploy:info',
+       'deploy:prepare',
+       'deploy:lock',
+       'deploy:release',
+       'deploy:update_code',
+       'deploy:shared',
+       'deploy:writable',
+       'deploy:vendors',
+   ];
+
+   foreach ($tasks as $task) {
+       invoke($task);
+   }
+})
+    ->desc('Install project');
+
+task('configure', function () {
+    invoke('prepare');
+
+    $tasks = [
+        'configure:database:cache_clear',
+        'configure:database:migrate',
+        'deploy:assets:install',
+        'configure:webpack',
+        'configure:cache:clear',
+        'configure:cache:warmup',
+    ];
+
+    foreach ($tasks as $task) {
+        invoke($task);
+    }
+})
+    ->desc('Configure project');
+
+task('link', function () {
+    invoke('prepare');
+
+    $tasks = [
+        'deploy:unlock',
+        'cleanup',
+    ];
+
+    foreach ($tasks as $task) {
+        invoke($task);
+    }
+})
+    ->desc('Link project');
 
 after('deploy:failed', 'deploy:unlock');
