5747f15f25faa6b65aedff8d42afd97604e4551d
ejjoj <szymaskibartosz@gmail.com>
2022-04-10 11:48:42 +0200

    deployer

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # php
-apt-get update
-apt-get upgrade -y
-apt-get install -y software-properties-common
-add-apt-repository ppa:ondrej/php
-apt-get update
-apt-get install -y php7.4 php7.4-mysql php7.4-zip php7.4-mbstring php7.4-json php7.4-intl php7.4-imap php7.4-fpm php7.4-cli php7.4-bz2 php7.4-bcmath php7.4-opcache php7.4-xml php7.4-xsl php7.4-curl php7.4-soap git
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository ppa:ondrej/php
+sudo apt-get update
+sudo apt-get install -y php7.4 php7.4-mysql php7.4-zip php7.4-mbstring php7.4-json php7.4-intl php7.4-imap php7.4-fpm php7.4-cli php7.4-bz2 php7.4-bcmath php7.4-opcache php7.4-xml php7.4-xsl php7.4-curl php7.4-soap git
 
 #node
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
@@ -21,28 +21,28 @@ echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/source
 sudo apt-get update && sudo apt-get install yarn
 
 # ip and ports
-apt-get install fail2ban
-ufw allow ssh
-ufw enable
-cp /etc/fail2ban/fail2ban.conf /etc/fail2ban/fail2ban.local
-cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+sudo apt-get install fail2ban
+sudo ufw allow ssh
+sudo ufw enable
+sudo cp /etc/fail2ban/fail2ban.conf /etc/fail2ban/fail2ban.local
+sudo cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
 
-fail2ban-client status
+sudo fail2ban-client status
 
-apt-get install -y iptables-persistent
+sudo apt-get install -y iptables-persistent
 
 # nginx
-apt-get install -y nginx-extras
-systemctl enable nginx.service #Auto-start for systemd
+sudo apt-get install -y nginx-extras
+sudo systemctl enable nginx.service #Auto-start for systemd
 
 # mysql
-apt-get install -y mysql-common mysql-server mysql-client
-mysql << EOF
+sudo apt-get install -y mysql-common mysql-server mysql-client
+sudo mysql << EOF
 CREATE DATABASE sdiwpil;
 CREATE USER 'sdiwpil'@'%' IDENTIFIED BY 'secret';
 GRANT ALL PRIVILEGES ON sdiwpil . * TO 'sdiwpil'@'%';
 FLUSH PRIVILEGES;
 EOF
 
-mysql_config_editor set --login-path=client --host=localhost --user=sdiwpil --password
-mysql_config_editor set --login-path=mysqldump --host=localhost --user=sdiwpil --password
+sudo mysql_config_editor set --login-path=client --host=localhost --user=sdiwpil --password
+sudo mysql_config_editor set --login-path=mysqldump --host=localhost --user=sdiwpil --password
