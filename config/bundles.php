a9172f3a178f247654902327071f71ed25cb9b53
ejjoj <szymaskibartosz@gmail.com>
2022-04-10 17:13:12 +0200

    wkhtmltopdf

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Sentry\SentryBundle\SentryBundle::class => ['all' => true],
+    Knp\Bundle\SnappyBundle\KnpSnappyBundle::class => ['all' => true],
 ];
