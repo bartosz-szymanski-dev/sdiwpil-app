0c30103ceed3d8726ce980f3bcb642c0ee0dafd7
ejjoj <szymaskibartosz@gmail.com>
2022-04-10 13:57:28 +0200

    deployer

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     FOS\JsRoutingBundle\FOSJsRoutingBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['all' => true],
 ];
