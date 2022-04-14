3c9a0149bd6d3e1ef84c89f6f8041f4ddaa78f79
ejjoj <szymaskibartosz@gmail.com>
2022-04-14 21:42:51 +0200

    prescription generation

--- src/Form/Document/DocumentType.php
+++ src/Form/Document/DocumentType.php
@@ -2,8 +2,11 @@
 
 namespace App\Form\Document;
 
+use App\Entity\DoctorData;
 use App\Entity\Document;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,15 +16,19 @@ class DocumentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class, [
+            ->add('type', ChoiceType::class, [
+                'required' => true,
+                'choices' => Document::ALLOWED_TYPES,
+            ])
+            ->add('doctor', EntityType::class, [
+                'required' => true,
+                'class' => DoctorData::class,
+            ])
+            ->add('patient', EntityType::class, [
                 'required' => true,
             ])
-            ->add('content', TextType::class, [
-
-            ])
-            ->add('type')
-            ->add('doctor')
-            ->add('patient');
+            ->add('prescription')
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
