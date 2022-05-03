55f23297afeb76e87e593d8a25aa36a536f50981
ejjoj <szymaskibartosz@gmail.com>
2022-05-03 11:33:02 +0200

    PaginatedRequestService.php - upgrade to php 8.1 features

--- src/Service/PaginatedRequestService.php
+++ src/Service/PaginatedRequestService.php
@@ -2,27 +2,21 @@
 
 namespace App\Service;
 
+use JetBrains\PhpStorm\ArrayShape;
 use RuntimeException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 
 class PaginatedRequestService
 {
-    private RequestStack $requestStack;
-
     private Request $request;
 
-    public function __construct(RequestStack $requestStack)
+    public function __construct(private readonly RequestStack $requestStack)
     {
-        $this->requestStack = $requestStack;
-
         $this->setRequest();
     }
 
-    /**
-     * @return array{min: int, max: int}
-     */
-    public function getMinMax(): array
+    #[ArrayShape(['min' => "float|int", 'max' => "int"])] public function getMinMax(): array
     {
         $page = (int)$this->request->get('page', 1);
         $perPage = (int)$this->request->get('per_page', 25);
