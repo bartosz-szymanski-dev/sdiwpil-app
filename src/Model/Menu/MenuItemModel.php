2163a236077925473fe07e1679fdc2325e2fef78
ejjoj <szymaskibartosz@gmail.com>
2022-05-03 11:49:53 +0200

    MenuItemModel.php - upgrade to php 8.1 features

--- src/Model/Menu/MenuItemModel.php
+++ src/Model/Menu/MenuItemModel.php
@@ -4,14 +4,10 @@ namespace App\Model\Menu;
 
 class MenuItemModel
 {
-    public string $route;
-    public string $name;
-    public string $icon;
-
-    public function __construct(string $route, string $name, string $icon)
-    {
-        $this->route = $route;
-        $this->name = $name;
-        $this->icon = $icon;
+    public function __construct(
+        public string $route,
+        public string $name,
+        public string $icon
+    ) {
     }
 }
