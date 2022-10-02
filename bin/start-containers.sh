e4b11d3fb4161fa26c8e672724013684f8f47d76
Bartek <szymaskibartosz@gmail.com>
2022-10-02 14:06:59 +0200

    [dockerize-project] code style change

--- bin/start-containers.sh
+++ bin/start-containers.sh
@@ -5,7 +5,7 @@ docker exec -it sdiwpil-back-end-service service nginx start -d
 docker exec -it sdiwpil-back-end-service service php8.1-fpm start -d
 docker exec -itd sdiwpil-front-end-service yarn dev
 
-declare -a nginx_containers_to_be_run=( "sdiwpil-back-end-service" "sdiwpil-nginx-service" "sdiwpil-front-end-service")
+declare -a nginx_containers_to_be_run=( "sdiwpil-back-end-service" "sdiwpil-nginx-service" "sdiwpil-front-end-service" )
 for nginx_container in "${nginx_containers_to_be_run[@]}"
 do
   docker exec -it "$nginx_container" service nginx start -d
